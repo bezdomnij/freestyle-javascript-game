579f7a9aac934bd3e4c75275e2d300f57c46707b
Atti <atti@otthon.megse.hu>
2020-05-20 23:59:12 +0200

    eat apples, not counting; stop at borders

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -1,38 +1,50 @@
 initGame();
 
 function initGame() {
-    selectField();
+    let appleWhere = placeApple();
     // Your game can start here, but define separate functions, don't write everything in here :)
-    moveHead();
+    moveHead(appleWhere);
 
 }
 
-function borderControl(){
+function borderControl() {
     //prevent illegal emigration
     let sHead = document.getElementsByClassName("snake_place");
-    if(sHead[0].offsetTop > 780){alert("Illegal border crossing!")}
-    if(sHead[0].offsetTop < 20){alert("Illegal border crossing!")};
-    if(sHead[0].offsetLeft < 18){alert("Illegal border crossing!")};
-    if(sHead[0].offsetLeft > 1258){alert("Illegal border crossing!")};
+    if (sHead[0].offsetTop > 780) {
+        alert("Illegal border crossing!")
+    }
+    if (sHead[0].offsetTop < 20) {
+        alert("Illegal border crossing!")
+    }
+    ;
+    if (sHead[0].offsetLeft < 18) {
+        alert("Illegal border crossing!")
+    }
+    ;
+    if (sHead[0].offsetLeft > 1258) {
+        alert("Illegal border crossing!")
+    }
+    ;
 
     // sHead[0].offsetLeft
 
 }
 
-function moveHead() {
-    // add event listener
+function moveHead(appleWhere) {// add event listener
+    console.log(appleWhere)
     let sHead_index;
     let bodySnatcher = document.querySelector('body');
     let sHead = document.getElementsByClassName("snake_place");
 
-    allFields=document.getElementsByClassName("field");
+    allFields = document.getElementsByClassName("field");
     console.log(allFields.length);
     console.log(sHead[0].className);
-    for (let i=0; i<allFields.length; i++) {
+    for (let i = 0; i < allFields.length; i++) {
         if (allFields.item(i).className === "field snake_place") {
             console.log("fuckit:", i);
             sHead_index = i;
-        };
+        }
+        ;
     }
 
     let rowCoordinate = sHead.item(0).getAttribute("data-row")
@@ -44,66 +56,99 @@ function moveHead() {
 
         if (e.keyCode === 38) { // up arrow
             console.log("up!");
-            // console.log(sHead);
-            // console.log(sHead[0].offsetTop);
-            // let snakeLocation = sHead[0].offsetTop;
-            // sHead[0].style.top = (snakeLocation - 40) + "px";
-            // borderControl()
             let new_pos = get_next_row_above(sHead_index);
-            allFields[new_pos].className = "field snake_place";
-            allFields[sHead_index].className = "field";
-            sHead_index = new_pos;
+            if (new_pos !== sHead_index) {
+                allFields[new_pos].className = "field snake_place";
+                allFields[sHead_index].className = "field";
+                sHead_index = new_pos;
+            }
+            if (new_pos === appleWhere) {
+                appleWhere = placeApple();
+            }
 
-        } else if (e.keyCode === 40) {
-            // down arrow
+        } else if (e.keyCode === 40) {// down arrow
             console.log("down!");
-            // let snakeLocation = sHead[0].offsetTop;
-            // sHead[0].style.top = (snakeLocation + 40) + "px";
-            // borderControl()
             let new_pos = get_next_row_below(sHead_index);
-            allFields[new_pos].className = "field snake_place";
-            allFields[sHead_index].className = "field";
-            sHead_index = new_pos;
+            if (new_pos !== sHead_index) {
+                allFields[new_pos].className = "field snake_place";
+                allFields[sHead_index].className = "field";
+                sHead_index = new_pos;
+            }
+            if (new_pos === appleWhere) {
+                appleWhere = placeApple();
+            }
 
-        } else if (e.keyCode === 37) {
-            // left arrow
+
+        } else if (e.keyCode === 37) {// left arrow
             console.log("left!");
-            let snakeLocation = sHead[0].offsetLeft;
-            sHead[0].style.left = (snakeLocation - 40) + "px";
-            borderControl()
-        } else if (e.keyCode === 39) {
-            // right arrow
+            let new_pos = get_next_col_left(sHead_index);
+            if (new_pos !== sHead_index) {
+                allFields[new_pos].className = "field snake_place";
+                allFields[sHead_index].className = "field";
+                sHead_index = new_pos;
+            }
+            if (new_pos === appleWhere) {
+                appleWhere = placeApple();
+            }
+
+        } else if (e.keyCode === 39) {// right arrow
             console.log("right!");
-            let snakeLocation = sHead[0].offsetLeft;
-            sHead[0].style.left = (snakeLocation + 40) + "px";
-            borderControl()
+            let new_pos = get_next_col_right(sHead_index);
+            if (new_pos !== sHead_index) {
+                allFields[new_pos].className = "field snake_place";
+                allFields[sHead_index].className = "field";
+                sHead_index = new_pos;
+            }
+            if (new_pos === appleWhere) {
+                appleWhere = placeApple();
+            }
+
         }
+
     });
 }
-    function get_next_row_above(current) {
-        let new_index = current - 20;
-        if (new_index < 0) {
-            new_index = current;
-        }
-        return new_index;
-    }
 
-    function get_next_row_below(current) {
-        let new_index = current + 20;
-        if (new_index > 300) {
-            new_index = current;
-        }
-        return new_index;
+function get_next_row_above(current) {
+    let new_index = current - 20;
+    if (new_index < 0) {
+        new_index = current;
     }
+    return new_index;
+}
 
-    function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
+function get_next_row_below(current) {
+    let new_index = current + 20;
+    if (new_index > 300) {
+        new_index = current;
     }
+    return new_index;
+}
 
-    function selectField() { // apple placement really
-        let fields = document.getElementsByClassName("field");
-        let randomInt = getRndInteger(1, 299);  //TODO: exclude snake coordinates
-        let apple = fields[randomInt]; // a lista random eleme
-        apple.className += "apple";
+function get_next_col_left(current) {
+    let new_index = current - 1;
+    if (new_index % 20 === 19) {
+        new_index = current;
     }
+    return new_index;
+}
+
+function get_next_col_right(current) {
+    let new_index = current + 1;
+    if (new_index % 20 === 0) {
+        new_index = current;
+    }
+    return new_index;
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function placeApple() { // apple placement really
+    let fields = document.getElementsByClassName("field");
+    let randomInt = getRndInteger(1, 299);  //TODO: exclude snake coordinates
+    let apple = fields[randomInt]; // a lista random eleme
+    apple.className += "apple"; // add 'apple' to class
+    return randomInt;
+}
 
