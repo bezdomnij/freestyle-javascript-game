49af8be5c28b6733f9e5163b11ae5121bddd3154
Atti <atti@otthon.megse.hu>
2020-05-23 11:23:35 +0200

    does something, but not that

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -1,79 +1,59 @@
+const allFields = document.getElementsByClassName("field");
+
+let sHead_index; // sneak head where
+let sIId = 0;
+let speed = 1000;
 initGame();
 
 function initGame() {
     let appleWhere = placeApple();
     // Your game can start here, but define separate functions, don't write everything in here :)
-    moveHead(appleWhere);
 
-}
-
-function borderControl() {
-    //prevent illegal emigration
-    let sHead = document.getElementsByClassName("snake_place");
-    if (sHead[0].offsetTop > 780) {
-        alert("Illegal border crossing!")
-    }
-    if (sHead[0].offsetTop < 20) {
-        alert("Illegal border crossing!")
-    }
-    ;
-    if (sHead[0].offsetLeft < 18) {
-        alert("Illegal border crossing!")
-    }
-    ;
-    if (sHead[0].offsetLeft > 1258) {
-        alert("Illegal border crossing!")
-    }
-    ;
-
-    // sHead[0].offsetLeft
-
-}
-
-function moveHead(appleWhere) {// add event listener
-    console.log(appleWhere)
-    let sHead_index;
-    let bodySnatcher = document.querySelector('body');
-    let sHead = document.getElementsByClassName("snake_place");
-
-    allFields = document.getElementsByClassName("field");
-    console.log(allFields.length);
-    console.log(sHead[0].className);
-    for (let i = 0; i < allFields.length; i++) {
+    for (let i = 0; i < allFields.length; i++) { // where is the head
         if (allFields.item(i).className === "field snake_place") {
-            console.log("fuckit:", i);
             sHead_index = i;
         }
-        ;
     }
+    // whereTo() => get_next_col_right(sHead_index);
+    // var t = setInterval(moveHead, 200, appleWhere);
+    moveHead(appleWhere);
+    sIId = setInterval(moveHead, speed, 12);
 
-    let rowCoordinate = sHead.item(0).getAttribute("data-row")
-    let columnCoordinate = sHead.item(0).getAttribute("data-col");
-    console.log(rowCoordinate, columnCoordinate);
+}
 
+function moveItMoveIt(whereTo) {
+
+    for (let i = 0; i < allFields.length; i++) { // where is the head
+        if (allFields.item(i).className === "field snake_place") {
+            sHead_index = i;
+        }
+    }
+    new_pos = whereTo(sHead_index);
+    if (new_pos !== sHead_index) { // border control: if new position then re-draw
+        allFields[new_pos].className = "field snake_place";
+        allFields[sHead_index].className = "field";
+        sHead_index = new_pos;
+    } else {
+        console.log("Elertel a falig");
+    }
+    return new_pos;
+}
+
+
+function moveHead(appleWhere) {// add event listener
+    const bodySnatcher = document.querySelector('body'); // event listener on this
     bodySnatcher.addEventListener('keydown', function (e) {
         // e = e || window.e;
-
         if (e.keyCode === 38) { // up arrow
             console.log("up!");
-            let new_pos = get_next_row_above(sHead_index);
-            if (new_pos !== sHead_index) {
-                allFields[new_pos].className = "field snake_place";
-                allFields[sHead_index].className = "field";
-                sHead_index = new_pos;
-            }
-            if (new_pos === appleWhere) {
-                appleWhere = placeApple();
+            moveItMoveIt(get_next_row_above);
+            if (new_pos === appleWhere) { // eat apple
+                appleWhere = placeApple(); // make new apple
             }
 
         } else if (e.keyCode === 40) {// down arrow
             console.log("down!");
-            let new_pos = get_next_row_below(sHead_index);
-            if (new_pos !== sHead_index) {
-                allFields[new_pos].className = "field snake_place";
-                allFields[sHead_index].className = "field";
-                sHead_index = new_pos;
-            }
+            moveItMoveIt(get_next_row_below);
             if (new_pos === appleWhere) {
                 appleWhere = placeApple();
             }
@@ -81,24 +61,14 @@ function moveHead(appleWhere) {// add event listener
 
         } else if (e.keyCode === 37) {// left arrow
             console.log("left!");
-            let new_pos = get_next_col_left(sHead_index);
-            if (new_pos !== sHead_index) {
-                allFields[new_pos].className = "field snake_place";
-                allFields[sHead_index].className = "field";
-                sHead_index = new_pos;
-            }
+            moveItMoveIt(get_next_col_left)
             if (new_pos === appleWhere) {
                 appleWhere = placeApple();
             }
 
         } else if (e.keyCode === 39) {// right arrow
             console.log("right!");
-            let new_pos = get_next_col_right(sHead_index);
-            if (new_pos !== sHead_index) {
-                allFields[new_pos].className = "field snake_place";
-                allFields[sHead_index].className = "field";
-                sHead_index = new_pos;
-            }
+            moveItMoveIt(get_next_col_right)
             if (new_pos === appleWhere) {
                 appleWhere = placeApple();
             }
@@ -142,17 +112,18 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function placeApple() { // apple placement really
+function placeApple() {
     let sHead_index, apple, randomInt;
     let fields = document.getElementsByClassName("field");
+
     for (let i = 0; i < fields.length; i++) {
         if (fields.item(i).className === "field snake_place") {
             console.log("fuckit:", i);
             sHead_index = i;
         }
     }
-    do {
-        randomInt = getRndInteger(0, 299);  //TODO: exclude snake coordinates
+    do { // check if apple on snake head
+        randomInt = getRndInteger(0, 299);
         apple = fields[randomInt]; // a lista random eleme
     } while (randomInt === sHead_index);
 
